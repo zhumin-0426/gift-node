1e1206e5513581d8455c8072b2f76f52744ea36a
zhumin <18820854754@139.com>
2021-03-11 17:18:25 +0800

    登入模块和注册模块的api接口

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = new express();
-// 前端模块
-let index = require('./routers/index');
-// 后台模块
+// admin module
 let admin = require('./routers/admin');
-app.use('/index', index);
 app.use('/admin', admin);
-//ejs模板引擎
-app.set('view engine', 'ejs');
-// public静态目录
-app.use(express.static('public'));
-// 图片的静态目录
+// picture static directory
 app.use('/upload',express.static('upload'));
-// 设置服务器端口
+// set service port
 let server = app.listen(8080, '127.0.0.1', function () {
     let host = server.address().address;
 
--- routers/admin/login/login.js
+++ routers/admin/login/login.js
@@ -6,31 +6,53 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 // 解析 application/json
 router.use(bodyParser.json());
-// 登入页面
-router.get('/', function (req, res) {
-    // res.render('admin/login')
-    console.log("这是登入模块");
-})
 // 登入
 router.post('/dologin', function (req, res) {
     let userLoginMsg = {
         adminAccount: req.body.adminAccount,
-        password: req.body.password
+        adminPassword: req.body.adminPassword
     }
+    //数据查询
     DB.findData('adminUsers', userLoginMsg, function (data) {
         if (data.length > 0) {
-            let data = {
-                status:"success"
-            }
-            res.json(data);
-            res.status(200);
+            res.json(data[0]);
+            res.end();
         } else {
-            res.send('<script>alert("登入失败");location.href="/admin/login"</script>')
+            res.status(404).send("I'm sorry! Login failed")
         }
     })
 })
 // 注册
-router.post('register',function(req,res){
-    console.log('req',req);
+router.post('/register', function (req, res, next) {
+    let userLoginMsg = {
+        adminAccount: req.body.adminAccount,
+        adminPassword: req.body.adminPassword
+    }
+    // 数据查询
+    DB.findData('adminUsers', userLoginMsg, function (data) {
+        if (data.length > 0) {
+            let registerStatus = {
+                registerStatus: "该账号已注册哦!"
+            }
+            res.json(registerStatus)
+        } else {
+            next()
+        }
+    })
+}, function (req, res) {
+    let userLoginMsg = {
+        adminAccount: req.body.adminAccount,
+        adminPassword: req.body.adminPassword
+    }
+    // 数据添加
+    DB.addData('adminUsers', userLoginMsg, function (data) {
+        const ops = data.ops;
+        if (ops.length > 0) {
+            let registerStatus = {
+                registerStatus: "账号注册成功哦!"
+            }
+            res.json(registerStatus)
+        }
+    })
 })
 module.exports = router;
